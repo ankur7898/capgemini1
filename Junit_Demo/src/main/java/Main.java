2d40fa2e455240cf85d78b18baaf97754686c7fc
ankur7898 <60469134+ankur7898@users.noreply.github.com>
2020-01-31 15:41:46 +0530

    Add files via upload

---
+++ Junit_Demo/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setId(101);
+		emp.setName("Ankur");
+		System.out.println(emp.getId() + emp.getName());
+	}
+}
